c6b5d8d74641ea5d61f5cbe160e9eee5fdb5698c
MatheusSFelix <xmatheusfelixx@hotmail.com>
2023-04-17 21:28:22 -0300

    Validações

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -1,19 +1,30 @@
 import { Link } from "react-router-dom";
 import imgCadastro from "../assets/img/img-cadastro2.svg";
-import { useState } from "react";
+import React, { useRef, useState } from "react";
 
 function Cadastro() {
+  const cpf = useRef();
+
   const [isActive, setActive] = useState(false);
   const prestador = () => {
-    setActive(true);
-  };
-
-  const cliente = () => {
     setActive(false);
   };
 
+  const cliente = () => {
+    setActive(true);
+  };
+
+  const mascaraCpf = () => {
+    console.log(cpf.current.value.length);
+    if (cpf.current.value.length === 3 || cpf.current.value.length === 7)
+      cpf.current.value += ".";
+
+    if (cpf.current.value.length === 3 || cpf.current.value.length === 11)
+      cpf.current.value += "-";
+  };
+
   return (
-    <div className="container">
+    <div className="container mb-3">
       <div className="row">
         <div className="col-md-6 d-flex justify-content-center align-content-center">
           <img src={imgCadastro} className="img-fluid" alt="" />
@@ -42,9 +53,10 @@ function Cadastro() {
                 Nome
               </label>
               <input
-                type="email"
+                type="text"
                 className="form-control"
                 placeholder="Informe seu nome"
+                onChange={mascaraCpf}
               />
             </div>
 
@@ -63,7 +75,7 @@ function Cadastro() {
                 Senha
               </label>
               <input
-                type="text"
+                type="password"
                 className="form-control"
                 placeholder="Informe sua senha"
               />
@@ -73,26 +85,111 @@ function Cadastro() {
                 Confirmar senha
               </label>
               <input
-                type="email"
+                type="password"
                 className="form-control"
                 placeholder="Confirmar senha"
               />
             </div>
+
+            <div className="mb-2">
+              <label htmlFor="" className="form-label">
+                CPF (apenas números)
+              </label>
+              <input
+                type="text"
+                onChange={mascaraCpf}
+                ref={cpf}
+                max={14}
+                className="form-control campo-cpf"
+                placeholder="Informe seu CPF"
+                disabled={cpf.length === 14 ? "true" : "false"}
+              />
+            </div>
+
+            <div className="mb-2">
+              <label htmlFor="" className="form-label">
+                RG
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Informe seu RG"
+              />
+            </div>
+
+            <div className="mb-2">
+              <label htmlFor="" className="form-label">
+                Gênero
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Informe seu CPF"
+              />
+            </div>
+
+            <div className="mb-2">
+              <label htmlFor="" className="form-label">
+                Telefone
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Informe seu telefone"
+              />
+            </div>
             {isActive ? (
-              <div className="mb-2">
-                <label htmlFor="" className="form-label">
-                  CPF
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Informe seu CPF"
-                />
-              </div>
+              <>
+                <h3 className="mt-3">Endereço</h3>
+                <div className="mb-2">
+                  <label htmlFor="" className="form-label">
+                    CEP
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Informe seu logradouro"
+                  />
+                </div>
+
+                <div className="row">
+                  <div className="mb-2 col-md-8">
+                    <label htmlFor="" className="form-label">
+                      Logradouro
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Informe seu logradouro"
+                    />
+                  </div>
+                  <div className="col-md-4">
+                    <label htmlFor="" className="form-label">
+                      Número
+                    </label>
+                    <input
+                      type="number"
+                      className="form-control"
+                      placeholder="nº"
+                    />
+                  </div>
+                </div>
+
+                <div className="mb-2">
+                  <label htmlFor="" className="form-label">
+                    Bairro
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Informe seu bairro"
+                  />
+                </div>
+              </>
             ) : (
               <span></span>
             )}
-            <button className="btn btn-primary w-100">Entrar</button>
+            <button className="btn btn-primary w-100 mt-3">Entrar</button>
           </form>
         </div>
       </div>
