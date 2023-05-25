6af25239ba402913afd5a2d728670560546ee60a
MatheusSFelix <xmatheusfelixx@hotmail.com>
2023-05-24 23:06:43 -0300

    iniciando notificação do cliente

--- src/components/NotificacaoCliente/ItemNotificacao.jsx
+++ src/components/NotificacaoCliente/ItemNotificacao.jsx
@@ -1,17 +1,32 @@
 /** @format */
-import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import axiosApi from "../../api/api";
 
-function ItemNotificacao() {
+function ItemNotificacao(props) {
+  const aprovar = () => {
+    console.log(props);
+    axiosApi
+      .put(`notificacoes/cliente/aprovar/${props.id}?aprovado=${true}`, null, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+      });
+  };
+
   return (
     <div className="item-notificacao d-flex mb-3">
       <div className="col-md-5 d-flex align-items-center">
         <div className="foto-perfil "></div>
-        <h4 className="ml-3">Fulano - Prestador</h4>
+        <h4 className="ml-3">{props.prestador}</h4>
       </div>
       <div className="col-md-7 d-flex justify-content-end align-items-center aceitar-recusar">
-        <Link className="btn btn-primary" to="">
+        <h4>Orçamento: R$ {props.orcamento}</h4>
+        <button className="btn btn-primary" onClick={() => aprovar()}>
           Aceitar
-        </Link>
+        </button>
         <button className="btn btn-secundary btn-recusar">Recusar</button>
       </div>
     </div>
--- src/pages/NotificacoesCliente.jsx
+++ src/pages/NotificacoesCliente.jsx
@@ -1,8 +1,40 @@
 /** @format */
 
 import ItemNotificacao from "../components/NotificacaoCliente/ItemNotificacao";
+import axiosApi from "../api/api";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function NotificacoesCliente() {
+  const [notificacoes, setNotificacoes] = useState();
+
+  useEffect(() => {
+    axiosApi
+      .get(`notificacoes/cliente/${localStorage.getItem("id")}`, {
+        headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+      })
+      .then((res) => {
+        let vetor = [];
+        console.log(res.data);
+        res.data.forEach((item, i) => {
+          console.log(item);
+          vetor.push(
+            <ItemNotificacao
+              key={i}
+              id={item.idCliente}
+              prestador={item.nomeCliente}
+              orcamento={item.valorOrcamento}
+            />
+          );
+          // console.log(item);
+        });
+
+        setNotificacoes(vetor);
+      });
+
+    // console.log(notificacoes.cliente);
+  }, []);
+
   return (
     <div>
       <div className="container conteudo mt-5 mb-5">
@@ -14,12 +46,7 @@ function NotificacoesCliente() {
           </div>
         </div>
         <div className="row">
-          <div className="col-md-12">
-            <ItemNotificacao />
-            <ItemNotificacao />
-            <ItemNotificacao />
-            <ItemNotificacao />
-          </div>
+          <div className="col-md-12">{notificacoes}</div>
         </div>
       </div>
     </div>
