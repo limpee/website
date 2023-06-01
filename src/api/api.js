d139d8157c7843dfcc287218cbca575a060d5897
MatheusSFelix <xmatheusfelixx@hotmail.com>
2023-06-01 09:54:53 -0300

    alterando url base

--- src/api/api.js
+++ src/api/api.js
@@ -4,8 +4,8 @@ import axios from "axios";
 
 const apiProjeto = axios.create({
   // baseURL: "https://643deb7a6c30feced81c335f.mockapi.io",
-  baseURL: "http://localhost:8080",
-  // baseURL: "https://backend-limpee.azurewebsites.net",
+  // baseURL: "http://localhost:8080",
+  baseURL: "https://backend-limpee.azurewebsites.net",
 });
 
 export default apiProjeto;
