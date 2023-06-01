db8927ae851e31e760414d6aa9c6ef0c1d43e89f
MatheusSFelix <xmatheusfelixx@hotmail.com>
2023-06-01 01:01:25 -0300

    Correção url

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,7 @@ import axios from "axios";
 const apiProjeto = axios.create({
   // baseURL: "https://643deb7a6c30feced81c335f.mockapi.io",
   // baseURL: "http://localhost:8080",
-  baseURL: "https:limpee.database.windows.net",
+  baseURL: "https://limpee.database.windows.net",
 });
 
 export default apiProjeto;
