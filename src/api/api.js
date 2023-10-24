a16ef0dc1c10a02d9353d60685442fdb6a3959dd
MatheusSFelixx <matheus.felix@sptech.school>
2023-10-24 15:23:54 -0300

    Alterando URL API

--- src/api/api.js
+++ src/api/api.js
@@ -5,8 +5,8 @@ import axios from "axios";
 const apiProjeto = axios.create({
   // baseURL: "https://643deb7a6c30feced81c335f.mockapi.io",
   //baseURL: "http://localhost:8080",
-  //baseURL: "https://backend-limpee.azurewebsites.net",
-  baseURL: "https://ec2-3-214-52-91.compute-1.amazonaws.com:8443",
+  baseURL: "https://backend-limpee.azurewebsites.net",
+  //baseURL: "https://ec2-3-214-52-91.compute-1.amazonaws.com:8443",
 });
 
 export default apiProjeto;
