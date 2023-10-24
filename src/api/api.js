8711e811451a6a2a4998788a683122b61618bef9
MatheusSFelixx <matheus.felix@sptech.school>
2023-10-24 14:06:40 -0300

    Mais ajustes na URL

--- src/api/api.js
+++ src/api/api.js
@@ -6,7 +6,7 @@ const apiProjeto = axios.create({
   // baseURL: "https://643deb7a6c30feced81c335f.mockapi.io",
   //baseURL: "http://localhost:8080",
   //baseURL: "https://backend-limpee.azurewebsites.net",
-  baseURL: "ec2-3-214-52-91.compute-1.amazonaws.com:8443",
+  baseURL: "https://ec2-3-214-52-91.compute-1.amazonaws.com:8443",
 });
 
 export default apiProjeto;
