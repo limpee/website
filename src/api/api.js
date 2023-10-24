df823ecea5a8707ef1d5211fe557252dcc702000
MatheusSFelixx <matheus.felix@sptech.school>
2023-10-24 14:03:37 -0300

    Ajustanto baseUrl

--- src/api/api.js
+++ src/api/api.js
@@ -4,8 +4,9 @@ import axios from "axios";
 
 const apiProjeto = axios.create({
   // baseURL: "https://643deb7a6c30feced81c335f.mockapi.io",
-  // baseURL: "http://localhost:8080",
-  baseURL: "https://backend-limpee.azurewebsites.net",
+  //baseURL: "http://localhost:8080",
+  //baseURL: "https://backend-limpee.azurewebsites.net",
+  baseURL: "ec2-3-214-52-91.compute-1.amazonaws.com:8443",
 });
 
 export default apiProjeto;
