4ce502c112f6cdca9092202a043e25106fdf2c5c
Matheus Felix <matheus.felix@alfaerp.com.br>
2024-08-15 18:51:36 -0300

    Ajusta notificações

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -64,6 +64,32 @@ function Login() {
             <br /> enquanto você <span>estava fora</span>!
           </p>
 
+          <form onSubmit={handleSubmit}>
+            <div className="mb-2">
+              <label htmlFor="" className="form-label">
+                E-mail
+              </label>
+              <input
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+                className="form-control"
+                placeholder="Informe seu e-mail"
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="" className="form-label">
+                Senha
+              </label>
+              <input
+                type="password"
+                onChange={(e) => setSenha(e.target.value)}
+                className="form-control"
+                placeholder="Informe sua senha"
+              />
+            </div>
+            <button className="btn btn-primary w-100">Entrar</button>
+            <Link to="/cadastro">Ainda não possui uma conta? Cadastre-se</Link>
+          </form>
         </div>
         <div className="col-md-6 d-flex justify-content-center align-content-center">
           <img src={imgLogin} className="img-fluid" alt="" />
--- src/pages/NotificacoesPrestador.jsx
+++ src/pages/NotificacoesPrestador.jsx
@@ -4,6 +4,66 @@ import NotificacaoAccordion from "../components/NotificacaoPrestador/Notificacao
 import axiosApi from "../api/api";
 import { useEffect } from "react";
 import { useState } from "react";
+function Mapa() {
+  let enderecos = [
+    "Rua das Flores, 123",
+    "Saldanho Marinho",
+    "Avenida Brasil, 4567",
+    "Rua XV de Novembro, 890",
+    "Rua dos Andradas, 2345",
+    "Avenida Paulista, 1500"
+  ];
+  const [notificacoes, setNotificacoes] = useState([]);
 
+  useEffect(() => {
+    axiosApi
+      .get(`notificacoes/prestador/${localStorage.getItem("id")}`, {
+        headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+      })
+      .then((res) => {
+        let vetor = [];
+        console.log(res);
+        res.data.forEach((item, i) => {
+          console.log(item);
+          vetor.push(
+            <NotificacaoAccordion
+              key={i}
+              id={"elemento" + i}
+              sub={"sub" + i}
+              formulario={item.formulario}
+              nome={item.cliente.nome}
+              idNotificacao={item.id}
+              endereco={
+                item.formulario.localServico === null
+                  ? `${item.cliente.endereco.logradouro}, ${item.cliente.endereco.numero}`
+                  : item.formulario.localServico
+              }
+            />
+          );
+        });
+
+        setNotificacoes(vetor);
+      });
+  }, [setNotificacoes]);
+
+  return (
+    <div>
+      <div className="accordion" id="notificacoesId">
+        <div className="container conteudo mt-5 mb-5">
+          <div className="row">
+            <div className="col-md-12">
+              <h2 className="title titulo-cards mb-4">
+                Notificações de orçamento
+              </h2>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12">{notificacoes}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Mapa;
